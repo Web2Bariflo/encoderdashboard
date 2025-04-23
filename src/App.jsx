c1859474184820cb67653d9c9588f7e6b1163b31
Web2Bariflo <web2.bariflo@outlook.com>
2025-04-23 18:14:28 +0530

    firtst push

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
               <div className="p-2 max-w-screen-xl mx-auto grid lg:grid-cols-3 gap-2">
                 {/* Chart Section */}
                 <div className="lg:col-span-2 bg-white shadow-sm rounded-xl p-6 transition duration-300">
-                  <h2 className="text-xl font-semibold mb-4 text-blue-600">Live Gear Chart</h2>
+                  <h2 className="text-xl font-semibold mb-4 text-blue-600">Live .Gear Chart</h2>
                   <Chart />
                 </div>
 
